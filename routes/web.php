0e189166e42524a02c61af5c623650deb90c0cbe
LAPTOP-TP6JE1SK\PowerTech <201810621@ua.edu.lb>
2021-12-04 18:29:41 +0200

    Job seeker application: add controller with create function/ Application form/ route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Auth.LoginScreen');
 });
+
+
+Route::get('/JobseekerAPP', function () {
+    return view('Jobseeker.JobseekerApp');
+});

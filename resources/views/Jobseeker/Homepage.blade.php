ebea591ad39aeb69f1574206638aa25e0da0010c
DESKTOP-7HEI2Q0\andre <201810293@ua.edu.lb>
2021-12-06 19:04:59 +0200

    JobProviderController functions

--- resources/views/Jobseeker/Homepage.blade.php
+++ resources/views/Jobseeker/Homepage.blade.php
@@ -1,90 +1,96 @@
 <!--display flex flex direction row align items center justify content between-->
 <!DOCTYPE html>
 <html>
-    <style>
+<style>
     .oval {
-     width: 160px;
-     height: 80px;
-     background: #fcfcfc;
-     border-radius: 40px;
-     border-block-color: #000000;
-        }
-.div-1{
-    width: 60%;
-   
-    background: #f7f5f5;
- box-align: "center";
- padding:20px 10px;
- border-radius: 20px;
- display: flex;
- flex-direction: row;
- align-items: center;
- justify-content: space-between;
-}
+        width: 160px;
+        height: 40px;
+        background: #fcfcfc;
+        border-radius: 40px;
+        border-block-color: #000000;
+    }
 
-    </style>
-    <body>
-        @csrf
-        
-        <table border="0" cellpadding="5" cellspacing="0" align="center">
-          
+    .div-1 {
+        width: 60%;
+        background: #f7f5f5;
+        box-align: "center";
+        padding: 20px 10px;
+        border-radius: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+</style>
+
+<body>
+    @csrf
+
+    <table border="0" cellpadding="5" cellspacing="0" align="center">
+
         <tr>
             <td style="width: 50%" align="center">
-                <form action="{{url('/search')}}">
-                <input class="oval" name="job" type="text" maxlength="50" style="width:100%;max-width: 260px" placeholder="What Job Title, Keywords, or company?"  required>
+                <form action="{{ url('/search') }}">
+                    <input class="oval" name="job" type="text" maxlength="50"
+                        style="width:100%;max-width: 260px" placeholder="What Job Title, Keywords, or company?"
+                        required>
                 </form>
-            </td> 
+            </td>
             <td>&nbsp;</td>
             <td style="width: 60%" align="center">
-              <form action="{{url('/search')}}">
-                <input class="oval" name="jobloc" type="text" maxlength="50" style="width:100%;max-width: 260px" placeholder="Where country, city, or zip code?" action="{{url('/search')}}" required>
+                <form action="{{ url('/search') }}">
+                    <input class="oval" name="jobloc" type="text" maxlength="50"
+                        style="width:100%;max-width: 260px" placeholder="Where country, city, or zip code?"
+                        action="{{ url('/search') }}" required>
                 </form>
-            </td>  
-        
-        </tr> 
-    
+            </td>
+
+        </tr>
+
     </table>
-<br><br>
+    <br><br>
 
 
-<div class="div-1">
-    <div class="profilepicture">
-        <img src="" alt="image">
+    <div class="div-1">
+        <div class="profilepicture">
+            <img src="" alt="image">
+
+        </div>
+        <div class="midcontainer">
+            <h2>frontend react native developper</h2>
+            <p>full time job(on site)</p>
+
+        </div>
+        <div class="rightcontainer">
+            <h3>Andre Solution</h3>
+            <p>Beirut-<a> andremawad.sarl.com</a></p>
+            <p>email:andre@mawad.com</p>
+        </div>
 
     </div>
-    <div class="midcontainer">
-        <h2>frontend react native developper</h2>
-        <p>full time job(on site)</p>
-        
-    </div>
-    <div class="rightcontainer">
-        <h3>Andre Solution</h3>
-        <p>Beirut-<a> andremawad.sarl.com</a></p>
-        <p>email:andre@mawad.com</p>
-    </div>
-    
-</div>
 
-{{--<table class="table table-bordered table-striped"  style="background-color:rgb(240, 240, 240)">
+    {{-- <table class="table table-bordered table-striped"  style="background-color:rgb(240, 240, 240)">
     <thead>
     </thead>
     <tbody>
         
-        @foreach ($Jobs as $key=>$job)
+        @foreach ($Jobs as $key => $job)
             
         <tr>
             <td>
-                {{--{{dd($job)}}--}}
-           {{--     {{$job[$key]->JobTitle}}
+                {{--{{dd($job)}} --}}
+    {{-- {{$job[$key]->JobTitle}}
            </td>
         </tr>
         
         @endforeach
 
     </tbody>
-</table>--}}
+</table> --}}
 
 
 
-    </body>
+</body>
+
 </html>

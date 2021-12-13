99c5e12dc1aad79fe7ccd811a581745012e2f86b
charbel ayrouth <ayrouthcharbel@gmail.com>
2021-12-13 18:33:13 +0200

    option index

--- resources/views/test/options/index.blade.php
+++ resources/views/test/options/index.blade.php
@@ -1,141 +1,57 @@
-@extends('layouts.admin')
+@extends('layouts.tailwind')
+@include('layouts.header')
 @section('content')
-@can('option_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route("admin.options.create") }}">
-                {{ trans('global.add') }} {{ trans('cruds.option.title_singular') }}
-            </a>
+    <div class="h-screen overflow-scroll my-30 flex justify-center" style="background: #edf2f7;">
+        <div class="w-2/3 mx-auto">
+            <div class="flex flex-col mt-20">
+                <div class="flex justify-between">
+                    <h2 class="font-bold text-2xl">question option</h2>
+                    <a href="{{ route('option.create', ['id' => $option->question_id]) }}"
+                        class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-green-500 hover:bg-green-dark no-underline">Create
+                        an Option</a>
+                </div>
+                <div class="bg-white shadow-md rounded my-6">
+                    <table class="text-left w-full border-collapse">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
+                                    ID</th>
+                                <th
+                                    class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
+                                    Option</th>
+                                <th
+                                    class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
+                                    Points</th>
+                                <th
+                                    class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
+                                    Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($options as $key => $option)
+                                <tr class="hover:bg-grey-lighter">
+                                    <td class="py-4 px-6 border-b border-grey-light">{{ $option->id }}</td>
+                                    <td class="py-4 px-6 border-b border-grey-light">{{ $option->option_text }}</td>
+                                    <td class="py-4 px-6 border-b border-grey-light">{{ $option->points }}</td>
+                                    <td class="py-4 px-6 border-b border-grey-light">
+                                        <a href=""
+                                            class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-green-500 hover:bg-green-dark no-underline">Edit</a>
+                                        <a href=""
+                                            class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-blue-500 hover:bg-blue-dark no-underline">View</a>
+                                        <form action="" method="post" class="inline">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit"
+                                                class="text-gray-100 font-bold py-1 px-3 rounded text-xs bg-red-500 hover:bg-red-dark no-underline">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.option.title_singular') }} {{ trans('global.list') }}
-    </div>
-
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class=" table table-bordered table-striped table-hover datatable datatable-Option">
-                <thead>
-                    <tr>
-                        <th width="10">
-
-                        </th>
-                        <th>
-                            {{ trans('cruds.option.fields.id') }}
-                        </th>
-                        <th>
-                            {{ trans('cruds.option.fields.question') }}
-                        </th>
-                        <th>
-                            {{ trans('cruds.option.fields.option_text') }}
-                        </th>
-                        <th>
-                            {{ trans('cruds.option.fields.points') }}
-                        </th>
-                        <th>
-                            &nbsp;
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($options as $key => $option)
-                        <tr data-entry-id="{{ $option->id }}">
-                            <td>
-
-                            </td>
-                            <td>
-                                {{ $option->id ?? '' }}
-                            </td>
-                            <td>
-                                {{ $option->question->question_text ?? '' }}
-                            </td>
-                            <td>
-                                {{ $option->option_text ?? '' }}
-                            </td>
-                            <td>
-                                {{ $option->points ?? '' }}
-                            </td>
-                            <td>
-                                @can('option_show')
-                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.options.show', $option->id) }}">
-                                        {{ trans('global.view') }}
-                                    </a>
-                                @endcan
-
-                                @can('option_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.options.edit', $option->id) }}">
-                                        {{ trans('global.edit') }}
-                                    </a>
-                                @endcan
-
-                                @can('option_delete')
-                                    <form action="{{ route('admin.options.destroy', $option->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                    </form>
-                                @endcan
-
-                            </td>
-
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-
-
-
-@endsection
-@section('scripts')
-@parent
-<script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('option_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.options.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
-
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
-
-        return
-      }
-
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
-
-  $.extend(true, $.fn.dataTable.defaults, {
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  $('.datatable-Option:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
-        $($.fn.dataTable.tables(true)).DataTable()
-            .columns.adjust();
-    });
-})
-
-</script>
 @endsection

47be15a02851eedef06f33b13f038028f5a1cb81
charbel ayrouth <ayrouthcharbel@gmail.com>
2021-12-13 13:57:20 +0200

    fixed result view

--- resources/views/Jobseeker/results.blade.php
+++ resources/views/Jobseeker/results.blade.php
@@ -9,27 +9,13 @@
     <title>Document</title>
 </head>
 
-<body>
-    {{-- <!DOCTYPE html> --}}
-    <html lang="en">
+<body class="h-screen flex items-center justify-center" style="background: #edf2f7;">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title>Edit</title>
-    </head>
+    <div class="w-6/12 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 class="mb-4 font-bold text-2xl">Results of your test</h1>
+        <p class="font-semibold">Total points: {{ $result->total_points }} points</p>
+    </div>
 
-    <body>
-        <div class="w-4/12 mx-auto  mt-20">
-            <h1>Results of your test</h1>
-            <div>Total points: {{ $result->total_points }} points</div>
-        </div>
-    </body>
-
-
-    </html>
 
 </body>
 
